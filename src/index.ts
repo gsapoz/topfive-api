5d2a4e3fdcfcaf02444d5edd582464fff808c0a7
Gary <gtech235@gmail.com>
2023-03-22 15:15:09 -0400

    init: restructured file system configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+export interface League {
+  name: string;
+  image?: string;
+  clubs: string[];
+}
 
+export interface Club {
+  name: string;
+  image: string;
+  players: string[];
+}
+
+export declare function allTeams(): string[];
+export declare function allLeagues(): string[];
