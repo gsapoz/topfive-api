0ba5ea766faa03a725b935adcf3d4f5cda4be4da
Gary <gtech235@gmail.com>
2023-03-23 14:08:12 -0400

    dev: removed test data from index

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,3 @@ export function allLeagues(): string[] {
   // Returns data for all top 5 football leagues in Europe
   return leagues;
 }
-
-//when you're finished, run npm tsc to convert this to javascript, that will create lib folder
-// https://www.youtube.com/watch?v=Nh9xW2-ZOEU
-// https://www.youtube.com/watch?v=vSxoV1KUF-A
