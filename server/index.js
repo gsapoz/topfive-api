7e7a4b8286fd004a5321420858320afc149677b8
Gary <gtech235@gmail.com>
2023-03-28 19:17:53 -0400

    Dev: implemented new fetch methods and node tests

--- server/index.js
+++ server/index.js
@@ -16,24 +16,106 @@ function upperCase(query) {
 
 function getAllClubs() {
   const data = loadJSON("../topfive.json");
+  let clubs = data[0].clubs;
 
-  return data;
+  clubs = clubs.concat(
+    data[1].clubs,
+    data[2].clubs,
+    data[3].clubs,
+    data[4].clubs
+  );
+
+  return clubs;
 }
 
 function getClubsFromLeague(league) {
   const data = loadJSON("../topfive.json");
   league = upperCase(league);
-
   let clubs = "";
+
   for (let i = 0; i < data.length; i++) {
     if (data[i].name.match(league)) {
       clubs = data[i].clubs;
     }
   }
+
   return clubs;
 }
+
+function getPlayersFromClub(club) {
+  const data = loadJSON("../topfive.json");
+  club = upperCase(club);
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].clubs.length; j++) {
+      if (data[i].clubs[j].name == club) {
+        return data[i].clubs[j].players;
+      }
+    }
+  }
+}
+
+function getPlayerAttributes(player) {}
+
+function getClubAttributes(club) {
+  const data = loadJSON("../topfive.json");
+  club = upperCase(club);
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].clubs.length; j++) {
+      if (data[i].clubs[j].name == club) {
+        return data[i].clubs[j];
+      }
+    }
+  }
+}
+
+function getLeagueAttributes(league) {
+  const data = loadJSON("../topfive.json");
+  league = upperCase(league);
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name.match(league)) {
+      return data[i];
+    }
+  }
+}
+
+function getLeagueImage(league) {
+  const data = loadJSON("../topfive.json");
+  league = upperCase(league);
+  let clubs = "";
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name.match(league)) {
+      return data[i].image;
+    }
+  }
+}
+
+function getClubImage(club) {
+  const data = loadJSON("../topfive.json");
+  club = upperCase(club);
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].clubs.length; j++) {
+      if (data[i].clubs[j].name == club) {
+        // console.log(data[i][j].image);
+        return data[i].clubs[j].image;
+        // break;
+      }
+    }
+  }
+}
+
+function getPlayerImage(player) {}
 
 module.exports = {
   getClubsFromLeague,
   getAllClubs,
+  getClubImage,
+  getLeagueImage,
+  getLeagueAttributes,
+  getClubAttributes,
+  getPlayersFromClub,
 };
--- server/test/test.js
+++ server/test/test.js
@@ -1,7 +1,19 @@
-const { getAllClubs, getClubsFromLeague } = require("../index");
+const {
+  getClubsFromLeague,
+  getAllClubs,
+  getClubImage,
+  getLeagueImage,
+  getLeagueAttributes,
+  getClubAttributes,
+  getPlayersFromClub,
+} = require("../index");
 
-const PL_Clubs = getClubsFromLeague("premier league");
+const PL_Clubs = getClubsFromLeague("premier league"); //returns all clubs from the PL, in one json object
+const All_Clubs = getAllClubs(); //returns all clubs in the list, concatanated together in one json object
+const Club_Image = getClubImage("chelsea"); //returns an image src for Chelsea's club crest
+const League_Image = getLeagueImage("la Liga"); //returns an image src for the crest of La Liga
+const League_Props = getLeagueAttributes("serie A"); //returns name, image, and clubs object for league
+const Club_Props = getClubAttributes("Villarreal CF"); //returns club name, crest image, and players from Villareal
+const Player_Props = getPlayersFromClub("Sevilla FC"); //returns a json object of all Sevilla players
 
-const All_Clubs = getAllClubs();
-
-console.log(PL_Clubs);
+console.log(Player_Props);
