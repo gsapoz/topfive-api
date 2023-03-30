5a33e64304fa1bf8d0d0e3f8b6d2984be5d10daa
Gary <gtech235@gmail.com>
2023-03-29 22:58:49 -0400

    fix: added relative path implementation to pass unit test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const data = loadJSON("../topfive.json");
+const data = loadJSON(__dirname + "/../topfive.json");
 
 function loadJSON(filename) {
   const data = fs.readFileSync(filename, "utf8");
